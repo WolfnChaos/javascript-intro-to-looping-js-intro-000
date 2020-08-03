7f73d80335bc0f425771c324830d6380c691c2dd
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 02:00:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,7 +13,11 @@ function forLoop(array) {
 n = 1
 function whileLoop(n) {
   while (n < 0) {
-    console.log(`I run once regardless.`);
-    n -= 1;
+      if (n === 1) {
+        console.log(`I run once regardless.`);
+        n -= 1;
+      } else {
+          return `done`
+      }
   }
 }
