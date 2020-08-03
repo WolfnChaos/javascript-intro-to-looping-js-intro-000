bd6977d8a4c9ab472f0662d511727f8b39aeabcc
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 01:29:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array) {
-  for (let i = 1; i < 26; i++) {
-    return array.slice('I am ${i} strange loop${i === 0 ? '' : 's'}.')
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
+      array.push('I am 1 strange loop.')
+    } else {
+      array.push('I am ${i} strange loops.')
+    }
   }
+  return array
 }
